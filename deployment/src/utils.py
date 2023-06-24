a26795aab8ec72a06e17b8329291e1252379692e
Ajay Sridhar <30612984+ajaysridhar0@users.noreply.github.com>
2023-06-24 16:01:46 -0700

    Fix resizing twice

--- deployment/src/utils.py
+++ deployment/src/utils.py
@@ -100,7 +100,7 @@ def transform_images(
     transform_type = transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Resize(image_size[::-1]),  
+            transforms.Resize(image_size),  
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
